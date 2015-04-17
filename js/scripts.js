55b304f11f7cb947ca05fb57cc2541e608c72c88
Brett <student@epicodus.com>
2015-04-17 10:09:48 -0700

    add jquery functionality to display pizza price

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,19 @@ var Pizza = {
         return 5 + (this.pizza_topping);
     }
 };
+
+$(document).ready(function() {
+    $("form#pizza").submit(function(event) {
+        event.preventDefault();
+
+        var slice = parseInt($('#pizza_topping').val());
+
+        var newPizza = Object.create(Pizza);
+
+        newPizza.person_age = slice;
+
+        var pizzaPrice = newPizza.priceCalc();
+
+        $(".result h3").text(pizzaPrice);
+    });
+});
